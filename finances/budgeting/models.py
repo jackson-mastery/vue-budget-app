cd6adb0eaa3dfb05d7340d6f10196703f48f03a6
Jackson Hammond <jhammond@masterytech.com>
2020-12-30 09:19:51 -0500

    Created first django models

--- finances/budgeting/models.py
+++ finances/budgeting/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=200)
+  cat_type = models.IntegerField(default=0)
+
+class Income(models.Model):
+  amount = models.IntegerField(default=0)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  date = models.DateField()
+  description = models.TextField()
+
+class Expense(models.Model):
+  amount = models.IntegerField(default=0)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  date = models.DateField()
+  description = models.TextField()
+
+class BudgetItem(models.Model):
+  amount = models.IntegerField(default=0)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  time_period = models.DateField() # Month/year this budget amount is for
+  description = models.TextField()
